7a897c5d61943f46eefad4472d87a8f032667d96
Nandini Shrivastav <shrivastav.n2002@gmail.com>
2026-01-14 15:20:03 +0530

    async practice

---
+++ async.js
@@ -0,0 +1,11 @@
+const { log } = require("async");
+
+async function getUser() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users/1");
+  const user = await response.json();
+  console.log(user.name);
+  console.log(user.email);
+  
+}
+
+getUser();
