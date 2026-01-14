df2e0c1071f07232ad1df9e23c87c8cf8d98a8f0
Nandini Shrivastav <shrivastav.n2002@gmail.com>
2026-01-14 17:23:57 +0530

    array methods

--- array.js
+++ array.js
@@ -12,3 +12,21 @@ const firstGreaterThanThree = numbers.find(num => num > 3);
 console.log(evenNumbers);
 console.log(squaredNumbers);
 console.log(firstGreaterThanThree);
+
+// slice and splice methods
+
+const add = numbers.slice(1,3); //output- 2,3
+
+const result = numbers.splice(0,2); // output- 3,4
+
+// sort and join methods
+
+const sortNums = numbers.sort((a,b)=> b-a); // output 4 3 2 1
+
+const joinNums = numbers.join("-"); // output 1-2-3-4
+
+// push and pop
+
+const pushNums = numbers.push(5,6); //output 1 2 3 4 5 6
+
+const popNums = numbers.pop(2); //output 1 3 4 
