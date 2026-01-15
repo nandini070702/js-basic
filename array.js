d03dcdc50791d95cb7b3ab807ce4b5f423ce86dd
Nandini Shrivastav <shrivastav.n2002@gmail.com>
2026-01-15 10:33:34 +0530

    changes

--- array.js
+++ array.js
@@ -15,18 +15,24 @@ console.log(firstGreaterThanThree);
 
 // slice and splice methods
 
-const add = numbers.slice(1,3); //output- 2,3
+const add = numbers.slice(1,3); 
+console.log("Sliced array:", add); //output- 2,3
 
-const result = numbers.splice(0,2); // output- 3,4
+const result = numbers.splice(0,2); 
+console.log("Spliced array:", result); // output- 1,2 
 
 // sort and join methods
 
-const sortNums = numbers.sort((a,b)=> b-a); // output 4 3 2 1
+const sortNums = numbers.sort((a,b)=> b-a); 
+console.log("sorted array:", sortNums);     // output 6, 5, 4, 3, 2, 1
 
-const joinNums = numbers.join("-"); // output 1-2-3-4
+const joinNums = numbers.join("-"); 
+console.log("joined array:", joinNums);  // output 1-2-3-4-5-6
 
 // push and pop
 
-const pushNums = numbers.push(5,6); //output 1 2 3 4 5 6
+const pushNums = numbers.push(7,8); 
+console.log("pushed array:", pushNums);   //output 1 2 3 4 5 6 7 8
 
-const popNums = numbers.pop(2); //output 1 3 4 
+const popNums = numbers.pop(2); 
+console.log("popped array:", popNums); //output 1 3 4 5 6 7 8
