7c62599e30495c239420acae5dcee0316ce81943
Nandini Shrivastav <shrivastav.n2002@gmail.com>
2026-01-15 14:13:05 +0530

    added env file

--- express-app/expbasic.js
+++ express-app/expbasic.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware: logs every request
 app.use((req, res, next) => {
